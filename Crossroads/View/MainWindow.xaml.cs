82c8bf7d5ea09a61a8c08f0069a0c44118fb9fab
Kargin Maxim <karginmaxx@gmail.com>
2023-10-28 22:06:07 +0300

    Создание главного окна

---
+++ Crossroads/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Crossroads
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
